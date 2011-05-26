3319a68f72015adae4dccd02b2325089d0a5aed6
J Kan <root@intangible-industries.com>
2011-05-26 13:31:45 -0700

    Added code to filter out self transitions to entropy calc

--- bigram.py
+++ bigram.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python
 
+# usage: ./bigram.py X36TrackDataS1S2-5-23-11 > entropy.csv
+
+
 import sys
 import csv
 import os
@@ -23,7 +26,7 @@ def bigrams(words):
 def probs(dict):
     """given a dictionary of counts, return a dict of probabilities"""
     sum = reduce(lambda x,y: x+y, dict.itervalues(), 0)
-    p = DefultDict(0)
+    p = DefaultDict(0)
     for (w, c) in dict.iteritems():
         p[w] = float(c)/sum
     return p
@@ -37,6 +40,12 @@ def probs_bigrams(dict):
         p[w] = probs(d)
     return p
 
+
+def remove_self_transition(zone, transitions): 
+    return dict([(k,v) for (k,v) in transitions.iteritems() if k != zone])
+
+def remove_self_transitions(allbigrams):
+    return dict([(zone, remove_self_transition(zone, transitions)) for (zone, transitions) in allbigrams.iteritems()])
 
 
 def h(p):
@@ -56,17 +65,24 @@ def zones(filename):
 
 
 def print_header():
-  print ",".join(["Handling", "Rat", "Session", "Zone Transitions", "Entropy"])
+  print ",".join(["Handling", "Rat", "Session", "Zone Transitions", "Entropy", "Between Zone Transitions", "Between Zone Entropy"])
 
 
 def process_file(filename, handling, rat, session): 
-  bi = bigrams(zones(filename))
+  allzones = zones(filename)
+
+  bi = bigrams(allzones)
   pp = probs_bigrams(bi)
+
+  between_zone_bi = remove_self_transitions(bi)
+  between_zone_pp = probs_bigrams(between_zone_bi)
 
   #print filename 
 
   total_count = 0
   total_entropy = 0
+  between_zone_count = 0
+  between_zone_entropy = 0
 
   for (p, zone) in pp.sorted():
     entropy = h(p.values())
@@ -74,12 +90,20 @@ def process_file(filename, handling, rat, session):
 
     total_count = total_count + count
     total_entropy = total_entropy + (count * entropy)
+
+    bz_p = between_zone_pp[zone]
+    bz_entropy = h(bz_p.values())
+    bz_count = sum(between_zone_bi[zone].values())
+
+    between_zone_count = between_zone_count + bz_count
+    between_zone_entropy = between_zone_entropy + (bz_count * bz_entropy)
+     
     
     details = ' '.join([ "%4s:%f" % pair for pair in p.iteritems()])
 
     #print "\t%4s : %4d * %f -> %4s" % (zone, count, entropy, details)
   
-  print ','.join([handling, rat, session, str(total_count), str(total_entropy)])
+  print ','.join([handling, rat, session, str(total_count), str(total_entropy), str(between_zone_count), str(between_zone_entropy)])
 
   #print "\tzone transitions:       %d" % total_count
   #print "\ttotal entropy:          %f" % total_entropy 
@@ -88,6 +112,9 @@ def process_file(filename, handling, rat, session):
 
 def main(argv=None):
   #root_dir = 'X36TrackDataS1S2-5-23-11'
+
+  if argv is None:
+      argv = sys.argv
 
   if len(argv) == 2:
       root_dir = argv[1]
--- graph.r
+++ graph.r
@@ -1,6 +1,7 @@
 library('ggplot2')
 
-h = read.csv("entropy.csv")
+h = read.csv("entropy-24-bz.csv")
 
-qplot(Zone.Transitions, Entropy, data=h, color=Handling, facets = ~ factor(Session), shape=Rat, size=6)
+qplot(Zone.Transitions,         Entropy, data=h, color=Handling, facets = ~Session, shape=Rat, size=6)
+qplot(Between.Zone.Transitions, Entropy, data=h, color=Handling, facets = ~Session, shape=Rat, size=6)
 
