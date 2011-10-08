ec38b92ae006ce0e098b9eea4bc7757f1f39f364
J Kan <root@intangible-industries.com>
2011-10-08 02:45:05 -0400

    latest round of graphs

--- graph.r
+++ graph.r
@@ -1,106 +1,255 @@
 library('ggplot2')
 
-h = read.csv("entropy-24-bz.csv")
+x36   = read.csv("X36TrackData-5-26-11/h.csv")
+x36ws = read.csv("X36TrackData-5-26-11/wse.csv")
+x36z  = read.csv("X36TrackData-5-26-11/infogain-by-zone.csv")
+
+xcu   = read.csv("X38TrackData-cppusl/h.csv")
+xcuws = read.csv("X38TrackData-cppusl/wse.csv")
+#xcuws6 = read.csv("X38TrackData-cppusl/wse6.csv")
+xcuz  = read.csv("X38TrackData-cppusl/infogain-by-zone.csv")
+
+xx = rbind(x36, xcu)
+xws = rbind(x36ws, xcuws)
+xz  = rbind(x36z, xcuz)
+
+#------- by session
+
+#--- treatment_vs_session boxplots
+
+ggplot(xx,   aes(x = factor(Session),
+                 y = Distance, 
+                 colour = Treatment, 
+                 shape = Rat)) + facet_grid(~Treatment) + geom_boxplot() + geom_jitter(size=3) + xlab("Session")
+
+ggplot(xx,   aes(x = factor(Session),
+                 y = Entropy, 
+                 colour = Treatment, 
+                 shape = Rat)) + facet_grid(~Treatment) + geom_boxplot() + geom_jitter(size=3) + xlab("Session")
+
+ggplot(xx,   aes(x = factor(Session),
+                 y = Cond.Entropy, 
+                 colour = Treatment, 
+                 shape = Rat)) + facet_grid(~Treatment) + geom_boxplot() + geom_jitter(size=3) + xlab("Session")
+
+ggplot(xx,   aes(x = factor(Session),
+                 y = Entropy/Distance, 
+                 colour = Treatment, 
+                 shape = Rat)) + facet_grid(~Treatment) + geom_boxplot() + geom_jitter(size=3) + xlab("Session")
 
 
-png("entropy-24-zones.png")
-qplot(Zone.Transitions,         Entropy, data=h, color=Handling, facets = ~Session, shape=Rat, size=6)
-dev.off()
+#--- treatment_vs_session lines + boxplots
 
-png("entropy-24-zones-between-zone.png")
-qplot(Between.Zone.Transitions, Entropy, data=h, color=Handling, facets = ~Session, shape=Rat, size=6)
-dev.off()
+ggplot(xx, aes(x       = Session,
+               y       = Distance, 
+               colour  = Treatment)) + 
+      facet_grid(~Treatment) + 
+      geom_boxplot(aes(group=factor(Session)), alpha=0.4, color="grey40", size=0.2) + 
+      geom_line(aes(group = Rat), size=1.5) + 
+      scale_x_continuous(breaks=c(1,2))
 
+ggplot(xx, aes(x       = Session,
+               y       = Entropy, 
+               colour  = Treatment)) + 
+      facet_grid(~Treatment) + 
+      geom_boxplot(aes(group=factor(Session)), alpha=0.4, color="grey40", size=0.2) + 
+      geom_line(aes(group = Rat), size=1.5) + 
+      scale_x_continuous(breaks=c(1,2))
 
+ggplot(xx, aes(x       = Session,
+               y       = Entropy/Distance, 
+               colour  = Treatment)) + 
+      facet_grid(~Treatment) + 
+      geom_boxplot(aes(group=factor(Session)), alpha=0.4, color="grey40", size=0.2) + 
+      geom_line(aes(group = Rat), size=1.5) + 
+      scale_x_continuous(breaks=c(1,2))
 
-hbz = read.csv("h-by-zone.csv")
+ggplot(xx, aes(x       = Session,
+               y       = Cond.Entropy,
+               colour  = Treatment)) + 
+      facet_grid(~Treatment) + 
+      geom_boxplot(aes(group=factor(Session)), alpha=0.4, color="grey40", size=0.2) + 
+      geom_line(aes(group = Rat), size=1.5) + 
+      scale_x_continuous(breaks=c(1,2))
 
-png("entropy-per-zone.png", width=2000, height=1000)
-qplot(Zone, Entropy, data=hbz, geom="jitter", facets = Session ~ Rat, shape = Rat, color=Handling)
-dev.off()
+ggplot(xx, aes(x       = Session,
+               y       = Cond.Entropy/Distance, 
+               colour  = Treatment)) + 
+      facet_grid(~Treatment) + 
+      geom_boxplot(aes(group=factor(Session)), alpha=0.4, color="grey40", size=0.2) + 
+      geom_line(aes(group = Rat), size=1.5) + 
+      scale_x_continuous(breaks=c(1,2))
 
-png("bz-entropy-per-zone.png", width=2000, height=1000)
-qplot(Zone, Between.Zone.Entropy, data=hbz, geom="jitter", facets = Session ~ Rat, shape = Rat, color=Handling)
-dev.off()
+ggplot(xx, aes(x       = Session,
+               y       = X.Zone.Transitions/Distance, 
+               colour  = Treatment)) + 
+      facet_grid(~Treatment) + 
+      geom_boxplot(aes(group=factor(Session)), alpha=0.4, color="grey40", size=0.2) + 
+      geom_line(aes(group = Rat), size=1.5) + 
+      scale_x_continuous(breaks=c(1,2))
 
-
-hbhb = cbind(hbz[hbz$Session == 1,], hbz[hbz$Session == 2,])
-names(hbhb)[i] = paste(names(hbhb)[i],(i-1) %/% 8 + 1, sep='.')
-
-hbhb$Handling.2 = NULL
-hbhb$Rat.2      = NULL
-hbhb$Zone.2     = NULL
-hbhb$Session.1  = NULL
-hbhb$Session.2  = NULL
-
-names(hbhb)[1]="Handling"
-names(hbhb)[2]="Rat"
-names(hbhb)[3]="Zone"
-
-
-hbhb$Diff.Entropy              = hbhb$Entropy.2 - hbhb$Entropy.1
-hbhb$Diff.Between.Zone.Entropy = hbhb$Between.Zone.Entropy.2 - hbhb$Between.Zone.Entropy.1
-hbhb$Diff.Occupancy            = hbhb$Occupancy.2 - hbhb$Occupancy.1
-hbhb$Diff.Out.Transitions      = hbhb$Out.Transitions.2 - hbhb$Out.Transitions.1
-
-
-png("diff-entropy.png", width=800, height=600)
-qplot(Handling, Diff.Entropy, data=hbhb, geom="jitter", facets = ~Zone, shape = Rat, color=Handling)
-dev.off()
-
-png("diff-between-zone-entropy.png", width=800, height=600)
-qplot(Handling, Diff.Between.Zone.Entropy, data=hbhb, geom="jitter", facets = ~Zone, shape = Rat, color=Handling)
-dev.off()
-
-
-
-png("diff-entropy-aggregate-over-zones.png", width=600, height=600)
-qplot(Handling, Diff.Entropy, data=hbhb, geom=c("boxplot","jitter"), shape = Rat, color=Handling)
-dev.off()
-
-png("diff-bz-entropy-aggregate-over-zones.png", width=600, height=600)
-qplot(Handling, Diff.Between.Zone.Entropy, data=hbhb, geom=c("boxplot","jitter"), shape = Rat, color=Handling)
-dev.off()
-
-png("diff-bz-transitions-over-zones.png", width=400, height=800)
-qplot(Handling, Diff.Out.Transitions, data=hbhb, geom=c("boxplot","jitter"), shape = Rat, color=Handling)
-dev.off()
+ggplot(xx, aes(x       = Session,
+               y       = X.Zone.Cond.Entropy/Distance,
+               colour  = Treatment)) + 
+      facet_grid(~Treatment) + 
+      geom_boxplot(aes(group=factor(Session)), alpha=0.4, color="grey40", size=0.2) + 
+      geom_line(aes(group = Rat), size=1.5) + 
+      scale_x_continuous(breaks=c(1,2))
 
 
-hag = aggregate(. ~ Rat, data=hbhb, "sum")
+#--- vs_distance
+                     
+ggd = ggplot(xx, aes(x = Distance,
+                     y = X.Zone.Cond.Entropy, 
+                     colour = Treatment,
+                     shape=Rat))
+
+ggd + facet_grid(Treatment~Session) + geom_boxplot(alpha=0.4, color="grey40", size=0.2) + stat_smooth(size=1, method="lm") + geom_point(size=5) 
+
+ggplot(xx, aes(x      = Distance,
+               y      = X.Zone.Transitions/Distance, 
+               colour = Treatment,
+               shape  = Rat)) +
+      facet_grid(Treatment~Session) + 
+      geom_boxplot(alpha=0.4, color="grey40", size=0.2) + 
+      geom_point(size=5) 
+
+ggplot(xx, aes(x      = Distance,
+               y      = Entropy/Distance, 
+               colour = Treatment,
+               shape  = Rat)) +
+      facet_grid(Treatment~Session) + 
+      geom_boxplot(alpha=0.4, color="grey40", size=0.2) + 
+      geom_point(size=5) 
 
 
-he1 = h[h$Session ==1 & h$Handling=='e',]
-he2 = h[h$Session ==2 & h$Handling=='e',]
-hn1 = h[h$Session ==1 & h$Handling=='n',]
-hn2 = h[h$Session ==2 & h$Handling=='n',]
+ggplot(xx, aes(x      = X.Zone.Transitions/Distance,
+               y      = Cond.Entropy/Distance, 
+               colour = Treatment,
+               group  = Session,     
+               shape  = Rat)) +
+      facet_grid(~Treatment) + 
+      geom_boxplot(alpha=0.4, color="grey40", size=0.2) + 
+      geom_line(aes(group = Rat)) + 
+      geom_point(aes(size=Session*4)) + scale_size(to=c(4,8), breaks=c(4,8), name="Session", labels=c("1","2"))
+
+#------- by epoch
+
+ggp = ggplot(xws, 
+             aes(x = Epoch,
+                 y = X.Zone.Transitions, 
+                 colour = Treatment, 
+                 shape = Rat,
+                 group = Rat))
+
+ggp + facet_grid(Treatment~Session) + geom_line()
+
+ggplot(xws, 
+       aes(x       = Epoch,
+           y       = Distance, 
+           colour  = Treatment)) + 
+    facet_grid(Treatment~Session) + 
+    geom_boxplot(aes(group=factor(Epoch)), alpha=0.4, color="grey40", size=0.2) + 
+    geom_line(aes(group = Rat), size=0.5) + 
+    scale_x_continuous(breaks=c(0,1,2))
 
 
-t.test(hn1$Entropy, hn2$Entropy, paired=TRUE, alternative="greater")
-t.test(hn1$Between.Zone.Entropy, hn2$Between.Zone.Entropy, paired=TRUE, alternative="greater")
+ggplot(xws, 
+       aes(x       = Epoch,
+           y       = Entropy, 
+           colour  = Treatment)) + 
+    facet_grid(Treatment~Session) + 
+    geom_boxplot(aes(group=factor(Epoch)), alpha=0.4, color="grey40", size=0.2) + 
+    geom_line(aes(group = Rat), size=0.5) + 
+    scale_x_continuous(breaks=c(0,1,2))
 
+ggplot(xws, 
+       aes(x       = Epoch,
+           y       = Cond.Entropy, 
+           colour  = Treatment)) + 
+    facet_grid(Treatment~Session) + 
+    geom_boxplot(aes(group=factor(Epoch)), alpha=0.4, color="grey40", size=0.2) + 
+    geom_line(aes(group = Rat), size=0.5) + 
+    scale_x_continuous(breaks=c(0,1,2))
 
+ggplot(xws, 
+       aes(x       = Epoch,
+           y       = X.Zone.Transitions, 
+           colour  = Treatment)) + 
+    facet_grid(Treatment~Session) + 
+    geom_boxplot(aes(group=factor(Epoch)), alpha=0.4, color="grey40", size=0.2) + 
+    geom_line(aes(group = Rat), size=0.5) + 
+    scale_x_continuous(breaks=c(0,1,2))
 
+ggplot(xws, 
+       aes(x       = Epoch,
+           y       = X.Zone.Cond.Entropy, 
+           colour  = Treatment)) + 
+    facet_grid(Treatment~Session) + 
+    geom_boxplot(aes(group=factor(Epoch)), alpha=0.4, color="grey40", size=0.2) + 
+    geom_line(aes(group = Rat), size=0.5) + 
+    scale_x_continuous(breaks=c(0,1,2))
 
+ggplot(xws, 
+       aes(x       = Epoch,
+           y       = Entropy/Distance, 
+           colour  = Treatment)) + 
+    facet_grid(Treatment~Session) + 
+    geom_boxplot(aes(group=factor(Epoch)), alpha=0.4, color="grey40", size=0.2) + 
+    geom_line(aes(group = Rat), size=0.5) + 
+    scale_x_continuous(breaks=c(0,1,2))
 
+ggplot(xws, 
+       aes(x       = Epoch,
+           y       = Cond.Entropy/Distance, 
+           colour  = Treatment)) + 
+    facet_grid(Treatment~Session) + 
+    geom_boxplot(aes(group=factor(Epoch)), alpha=0.4, color="grey40", size=0.2) + 
+    geom_line(aes(group = Rat), size=0.5) + 
+    scale_x_continuous(breaks=c(0,1,2))
 
-hbye = read.csv("wse.csv")
+ggplot(xws, 
+       aes(x       = Epoch,
+           y       = X.Zone.Transitions/Distance, 
+           colour  = Treatment)) + 
+    facet_grid(Treatment~Session) + 
+    geom_boxplot(aes(group=factor(Epoch)), alpha=0.4, color="grey40", size=0.2) + 
+    geom_line(aes(group = Rat), size=0.5) + 
+    scale_x_continuous(breaks=c(0,1,2))
 
-png("h-by-epoch.png",width=500,height=600)
-qplot(factor(Epoch), Entropy, data=hbye, facets=Handling~Session, color=Handling, shape=Rat, geom=c("boxplot","jitter"))
-dev.off()
+ggplot(xws, 
+       aes(x       = Epoch,
+           y       = X.Zone.Cond.Entropy/Distance, 
+           colour  = Treatment)) + 
+    facet_grid(Treatment~Session) + 
+    geom_boxplot(aes(group=factor(Epoch)), alpha=0.4, color="grey40", size=0.2) + 
+    geom_line(aes(group = Rat), size=0.5) + 
+    scale_x_continuous(breaks=c(0,1,2))
 
-png("bz-transitions-by-epoch.png",width=500,height=600)
-qplot(factor(Epoch), Between.Zone.Transitions, data=hbye, facets=Handling~Session, color=Handling, shape=Rat, geom=c("boxplot","jitter"))
-dev.off()
+    
+#------- by zone
+    
+ggplot(xz, aes(x      = Session,
+               y      = Occupancy, 
+               colour = Treatment, 
+               group  = Rat)) +
+    facet_grid(~Zone) + 
+    geom_line() + scale_x_continuous(limits = c(1,2), breaks=c(1,2)) 
 
-hbye3 = read.csv("wse3.csv")
+ggplot(xz, aes(x      = Session,
+               y      = Occupancy, 
+               colour = Treatment, 
+               group  = Rat)) +
+    facet_grid(~Zone) + 
+    geom_line(alpha=0.3) + 
+    scale_x_continuous(limits = c(1,2), breaks=c(1,2)) 
 
-png("bz-transitions-by-3-epochs.png",width=500,height=600)
-qplot(factor(Epoch), Between.Zone.Transitions, data=hbye3, facets=Handling~Session, color=Handling, shape=Rat, geom=c("boxplot","jitter"))
-dev.off()
+ggpz = ggplot(xz, 
+              aes(x = factor(Session),
+                  y = Entropy, 
+                  colour = Treatment, 
+                  shape = Rat))
 
-png("h-by-3-epochs.png",width=500,height=600)
-qplot(factor(Epoch), Entropy, data=hbye3, facets=Handling~Session, color=Handling, shape=Rat, geom=c("boxplot","jitter"))
-dev.off()
+ggpz + facet_grid(~Zone) + geom_boxplot() + geom_jitter()
 
+    
